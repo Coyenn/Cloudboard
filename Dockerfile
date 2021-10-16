69d05d7a0dc7fa36faf971d76889a5331a442ea1
Coyenn <timrittermail@gmail.com>
2021-10-16 18:59:57 +0200

    Small adjustments

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ COPY ./ /app
 WORKDIR /app
 RUN yarn install && yarn build
 EXPOSE 80
+
 CMD [ "/bin/bash", "cd /app && yarn start" ]
--- data/routes.js
+++ data/routes.js
@@ -1,6 +1,5 @@
 const routes = [
 	{ title: "Dashboard", route: "/" },
-	{ title: "Config", route: "/about" },
 ];
 
 export default routes;
