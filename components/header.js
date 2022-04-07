6bd55c498a24aadaf321a28c2dbc2ef1907e6a69
Tim Ritter <timrittermail@gmail.com>
2022-04-07 18:16:44 +0000

    Remove duplicate class

--- components/header.js
+++ components/header.js
@@ -38,7 +38,7 @@ export default function Header() {
           </Link>
         </div>
 
-        <button className="flex items-center block px-3 py-2 text-white border border-white rounded md:hidden" onClick={() => setMobileMenuIsOpen(!mobileMenuIsOpen)}>
+        <button className="flex items-center px-3 py-2 text-white border border-white rounded md:hidden" onClick={() => setMobileMenuIsOpen(!mobileMenuIsOpen)}>
           <svg className="w-3 h-3 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
